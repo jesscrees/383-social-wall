523453eeaa3ce8ecefe20905bfa37aa9acb04643
Jess Crees <jessicacrees@gmail.com>
2019-10-17 21:10:14 +0100

    Added index routing to server as per the steps in this tutorial: http://coding-karma.com/2018/10/22/deploy-your-angular-5-application-to-heroku/

--- server.js
+++ server.js
@@ -6,5 +6,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/social-wall'));
 
+//Index routing 
+app.get('/*', (req, res)=>{
+  res.sendFile(path.join(__dirname + '/dist/social-wall/index.html'));
+});
+
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
